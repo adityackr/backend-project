ca8d4ae5fddce4929007cc3910b560b95dee2a4f
adityackr <aditya09me@gmail.com>
2025-10-25 16:27:46 +0600

    feat: add cookie-parser and cors middleware, implement basic Express server setup
    - Added cookie-parser and cors as dependencies in package.json.
    - Created app.js to set up Express server with middleware for CORS, JSON parsing, URL encoding, and static file serving.
    - Updated index.js to connect to the database and start the server, handling errors appropriately.
    - Introduced ApiError and ApiResponse utility classes for standardized error and response handling.
    - Added asyncHandler utility for handling asynchronous route handlers with error catching.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,21 @@
 import 'dotenv/config';
+import { app } from './app.js';
 import connectDB from './db/index.js';
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.on('error', (error) => {
+      console.error('Express server error:', error);
+      throw error;
+    });
+
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running on port ${process.env.PORT || 8000}`);
+    });
+  })
+  .catch((error) => {
+    console.error('Failed to connect to the database:', error);
+  });
 
 // import express from 'express';
 // const app = express();
