544fb1a81ea85829d1bd9a3e5e82ae4c69ab5f08
adityackr <aditya09me@gmail.com>
2025-10-27 21:30:26 +0600

    feat: implement user and video models with authentication and pagination support

---
+++ src/models/user.model.js
@@ -0,0 +1,71 @@
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import { model, Schema } from 'mongoose';
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      index: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    fullName: { type: String, required: true, trim: true, index: true },
+    avatar: { type: String, required: true },
+    coverImage: { type: String },
+    watchHistory: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Video',
+      },
+    ],
+    password: { type: String, required: [true, 'Password is required'] },
+    refreshToken: { type: String },
+  },
+  { timestamps: true }
+);
+
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+userSchema.methods.isPasswordMatch = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+userSchema.methods.generateAccessToken = function () {
+  jwt.sign(
+    {
+      _id: this._id,
+      username: this.username,
+      email: this.email,
+      fullName: this.fullName,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    { expiresIn: process.env.ACCESS_TOKEN_EXPIRES_IN }
+  );
+};
+
+userSchema.methods.generateRefreshToken = function () {
+  jwt.sign(
+    {
+      _id: this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    { expiresIn: process.env.REFRESH_TOKEN_EXPIRES_IN }
+  );
+};
+
+export const User = model('User', userSchema);
